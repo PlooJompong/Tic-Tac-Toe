91cf4e67f33e74e7e9144e3a2e1e8a492bc0055d
ponjom2 <ponjom2@hotmail.com>
2022-09-15 14:24:58 +0200

    Base game done
    TODO
     - Add Score
     - New layout//design?
     - Remove Restart Button?
     - Cleaning

--- Tic Tac Toe/ViewController/MainController.swift
+++ Tic Tac Toe/ViewController/MainController.swift
@@ -5,6 +5,14 @@
 //  Created by Ploo Jompong on 2022-09-08.
 //
 
+/*  TODO
+ - Add Score
+ - New layout//design?
+ - Remove Restart Button?
+ - Cleaning
+*/
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,20 +22,7 @@ class ViewController: UIViewController {
     
     // Game Board
     @IBOutlet var buttonCollection: [UIButton]!
-    
-    // Game Board
-    /*
-     @IBOutlet weak var a1: UIButton!
-     @IBOutlet weak var a2: UIButton!
-     @IBOutlet weak var a3: UIButton!
-     @IBOutlet weak var b1: UIButton!
-     @IBOutlet weak var b2: UIButton!
-     @IBOutlet weak var b3: UIButton!
-     @IBOutlet weak var c1: UIButton!
-     @IBOutlet weak var c2: UIButton!
-     @IBOutlet weak var c3: UIButton!
-     */
-    
+
     // Turn Label
     @IBOutlet weak var turnLbl: UILabel!
     
@@ -37,99 +32,154 @@ class ViewController: UIViewController {
     // Reset Button
     @IBOutlet weak var resetBtn: UIButton!
     
-    // Current Turn: true = Player 1 (X)
+    // Current turn: true = Player 1 (X)
     var currentTurn: Bool = true
     
+    // Game stared = false
+    var gameStarted: Bool = false
+    
+    // X && O
     var x: String = "X"
     var o: String = "O"
     
-    //lazy var board = [a1, a2, a3, b1, b2, b3, c1, c2, c3]
-    lazy var board = [buttonCollection]
+    lazy var board = [UIButton]()
     
-    // Custom Button Title Styles
-    lazy var customButtonTitleX = NSMutableAttributedString(string: x, attributes: [
-        NSAttributedString.Key.font: UIFont.systemFont(ofSize: 60, weight: .heavy),
-        NSAttributedString.Key.backgroundColor: UIColor.white,
-        //NSAttributedString.Key.foregroundColor: UIColor.black,
-        NSAttributedString.Key.foregroundColor: UIColor(red: 00/255.00, green: 197/255.00, blue: 31/255.00, alpha: 0.5)
-    ])
-    lazy var customButtonTitleO = NSMutableAttributedString(string: o, attributes: [
-        NSAttributedString.Key.font: UIFont.systemFont(ofSize: 60, weight: .heavy),
-        NSAttributedString.Key.backgroundColor: UIColor.white,
-        //NSAttributedString.Key.foregroundColor: UIColor.black,
-        NSAttributedString.Key.foregroundColor: UIColor(red: 239/255.00, green: 83/255.00, blue: 80/255.00, alpha: 0.5)
-    ])
-    
-    // Custom Turn Styles
+    // Custom label styles
     lazy var customTurnX = NSMutableAttributedString(string: x, attributes: [
         NSAttributedString.Key.font: UIFont.systemFont(ofSize: 32, weight: .heavy),
-        NSAttributedString.Key.backgroundColor: UIColor.white,
-        //NSAttributedString.Key.foregroundColor: UIColor.black
         NSAttributedString.Key.foregroundColor: UIColor(red: 00/255.00, green: 197/255.00, blue: 31/255.00, alpha: 0.5)
     ])
     lazy var customTurnO = NSMutableAttributedString(string: o, attributes: [
         NSAttributedString.Key.font: UIFont.systemFont(ofSize: 32, weight: .heavy),
-        NSAttributedString.Key.backgroundColor: UIColor.white,
-        //NSAttributedString.Key.foregroundColor: UIColor.black
         NSAttributedString.Key.foregroundColor: UIColor(red: 239/255.00, green: 83/255.00, blue: 80/255.00, alpha: 0.5)
     ])
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        styleLayout()
+        //styleLayout()
     }
     
-    func styleLayout() {
-        for button in buttonCollection {
-            button.layer.cornerRadius = 50.0
-            //button?.clipsToBounds = true
+    // On board tap
+    @IBAction func onBoardTap(_ sender: UIButton) {
+        onTap(sender)
+        checkWinner()
+        if fullBoard() {
+            drawAlert()
         }
     }
     
-    @IBAction func onTap(_ sender: UIButton) {
-        if sender.title(for: .normal) == nil {
-            if currentTurn {
-                //print(x)
+    // Place X || O
+     func onTap(_ sender: UIButton) {
+         if sender.title(for: .normal) == nil {
+             if currentTurn {
+                 turnLbl.attributedText = customTurnO
+                 sender.setTitle(x, for: .normal)
+                 sender.setTitleColor(UIColor(red: 00/255, green: 197/255, blue: 31/255, alpha: 0.5), for: .normal)
+                 currentTurn = false
+             } else {
+                 turnLbl.attributedText = customTurnX
+                 sender.setTitle(o, for: .normal)
+                 sender.setTitleColor(UIColor(red: 239/255.00, green: 83/255.00, blue: 80/255.00, alpha: 0.5), for: .normal)
+                 currentTurn = true
+             }
+             sender.isEnabled = false
+             gameStarted = true
+         }
+     }
+    
+    // Check if the borad is full
+    func fullBoard() -> Bool {
+        for button in buttonCollection {
+            if button.title(for: .normal) == nil {
+                return false
+            }
+        }
+        return true
+    }
+    
+    // Win conditions
+    func checkWinner() {
+        // X win condition
+        if ((buttonCollection[0].title(for: .normal) == x && buttonCollection[1].title(for: .normal) == x && buttonCollection[2].title(for: .normal) == x) ||
+            (buttonCollection[3].title(for: .normal) == x && buttonCollection[4].title(for: .normal) == x && buttonCollection[5].title(for: .normal) == x) ||
+            (buttonCollection[6].title(for: .normal) == x && buttonCollection[7].title(for: .normal) == x && buttonCollection[8].title(for: .normal) == x) ||
+            (buttonCollection[0].title(for: .normal) == x && buttonCollection[3].title(for: .normal) == x && buttonCollection[6].title(for: .normal) == x) ||
+            (buttonCollection[1].title(for: .normal) == x && buttonCollection[4].title(for: .normal) == x && buttonCollection[7].title(for: .normal) == x) ||
+            (buttonCollection[2].title(for: .normal) == x && buttonCollection[5].title(for: .normal) == x && buttonCollection[8].title(for: .normal) == x) ||
+            (buttonCollection[0].title(for: .normal) == x && buttonCollection[4].title(for: .normal) == x && buttonCollection[8].title(for: .normal) == x) ||
+            (buttonCollection[2].title(for: .normal) == x && buttonCollection[4].title(for: .normal) == x && buttonCollection[6].title(for: .normal) == x)) {
+            let alert = UIAlertController(title: "X win", message: nil, preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "Restart", style: .default, handler: { (_) in self.resetBoard() }))
+            self.present(alert, animated: true)
+        }
+        
+        // O win condition
+        else if ((buttonCollection[0].title(for: .normal) == o && buttonCollection[1].title(for: .normal) == o && buttonCollection[2].title(for: .normal) == o) ||
+                 (buttonCollection[3].title(for: .normal) == o && buttonCollection[4].title(for: .normal) == o && buttonCollection[5].title(for: .normal) == o) ||
+                 (buttonCollection[6].title(for: .normal) == o && buttonCollection[7].title(for: .normal) == o && buttonCollection[8].title(for: .normal) == o) ||
+                 (buttonCollection[0].title(for: .normal) == o && buttonCollection[3].title(for: .normal) == o && buttonCollection[6].title(for: .normal) == o) ||
+                 (buttonCollection[1].title(for: .normal) == o && buttonCollection[4].title(for: .normal) == o && buttonCollection[7].title(for: .normal) == o) ||
+                 (buttonCollection[2].title(for: .normal) == o && buttonCollection[5].title(for: .normal) == o && buttonCollection[8].title(for: .normal) == o) ||
+                 (buttonCollection[0].title(for: .normal) == o && buttonCollection[4].title(for: .normal) == o && buttonCollection[8].title(for: .normal) == o) ||
+                 (buttonCollection[2].title(for: .normal) == o && buttonCollection[4].title(for: .normal) == o && buttonCollection[6].title(for: .normal) == o)) {
+            let alert = UIAlertController(title: "O win", message: nil, preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "Restart", style: .default, handler: { (_) in self.resetBoard() }))
+            self.present(alert, animated: true)
+        }
+        
+    }
+    
+    // Draw alert
+    func drawAlert() {
+        let alert = UIAlertController(title: "DRAW", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Reset", style: .default, handler: { (_) in self.resetBoard() }))
+        self.present(alert, animated: true)
+    }
+    
+    // Switch starting player
+    @IBAction func switchStartPlayer(_ sender: UIButton) {
+        if gameStarted == false {
+            let alert = UIAlertController(title: "Switch Starting Player", message: nil, preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alert, animated: true)
+            if currentTurn == true {
                 turnLbl.attributedText = customTurnO
-                sender.setAttributedTitle(customButtonTitleX, for: .normal)
                 currentTurn = false
             } else {
-                //print(o)
                 turnLbl.attributedText = customTurnX
-                sender.setAttributedTitle(customButtonTitleO, for: .normal)
                 currentTurn = true
             }
-            sender.isEnabled = false
+        } else {
+            let alert = UIAlertController(title: "Game Already Started", message: nil, preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alert, animated: true, completion: nil)
         }
     }
     
-    @IBAction func switchStartPlayer(_ sender: UIButton) {
-        startWithPlayerTwoAlert()
-        turnLbl.attributedText = customTurnO
-        currentTurn = false
-    }
-    
-    func startWithPlayerTwoAlert() {
-        let alert = UIAlertController(title: "Start with Player 2", message: "", preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-            //NSLog("The \"OK\" alert occured.")
-            NSLog("Start with Player 2")
-        }))
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-    
-    // TODO - Reset Button
-    @IBAction func resetOnTap(_ sender: UIButton) {
+    // Reset board
+    func resetBoard() {
         for button in buttonCollection {
             button.setTitle(nil, for: .normal)
-            sender.isEnabled = true
-            print("loop")
+            button.isEnabled = true
+            gameStarted = false
         }
-        print("2")
-        //sender.isEnabled = true
-        turnLbl.attributedText = customTurnX
-        currentTurn = true
     }
 }
+    
+//    @IBAction func restartOnTap(_ sender: UIButton) {
+//        resetBoard()
+//        if currentTurn == true {
+//            turnLbl.attributedText = customTurnX
+//            currentTurn = true
+//        } else {
+//            turnLbl.attributedText = customTurnO
+//            currentTurn = false
+//        }
+//    }
 
+//    func styleLayout() {
+//        for button in buttonCollection {
+//            button.layer.cornerRadius = 50.0
+//            //button?.clipsToBounds = true
+//        }
+//    }
